0658032698133071ec6d03c2c7c4f243843ddbe6
jjargot <jerome.jargot@bonitasoft.com>
2018-03-09 14:10:17 +0100

    Initial commit of 'phiphi'

---
+++ src-connectors/org/support/bonitasoft/connector/AbstractTestcImpl.java
@@ -0,0 +1,13 @@
+package org.support.bonitasoft.connector;
+
+import org.bonitasoft.engine.connector.AbstractConnector;
+import org.bonitasoft.engine.connector.ConnectorValidationException;
+
+public abstract class AbstractTestcImpl extends AbstractConnector {
+
+	@Override
+	public void validateInputParameters() throws ConnectorValidationException {
+
+	}
+
+}
